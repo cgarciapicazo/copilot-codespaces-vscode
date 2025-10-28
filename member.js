c336eada61e1359ab1c30d1edc4fc1a8c414be2e
Carlos Picazo <105385064+cgarciapicazo@users.noreply.github.com>
2025-10-28 22:48:50 +0000

    Github Second Commit

---
+++ member.js
@@ -0,0 +1,8 @@
+    function skillsMember() {
+        console.log("This is the skills member function.");
+    }
+
+    function experienceMember() {
+        console.log("This is the experience member function.");
+    }
+    
